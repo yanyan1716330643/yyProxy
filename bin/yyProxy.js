7954a7ede9690302810d199de2c99c3fd39e49e9
yanyan <1716330643@qq.com>
2017-12-05 20:09:40 +0800

    write package.json

--- bin/yyProxy.js
+++ bin/yyProxy.js
@@ -3,7 +3,8 @@ let http = require("http");
 let httpProxy = require("http-proxy");
 let proxy = httpProxy.createProxy({});
 let filePath = (process.argv && process.argv[2]) || "../proxyConfig";
-let proxyConfig = require(filePath);
+let path = require("path")
+let proxyConfig = require(path.join(filePath));
 for(let obj of proxyConfig){
     let server = http.createServer(function (req, res) {
         proxy.web(req,res,{target:obj.targetUrl});
