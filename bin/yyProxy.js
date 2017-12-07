accb428499b6e809ad3727c382c8bdf6502ac9db
yanyan <1716330643@qq.com>
2017-12-07 11:20:29 +0800

    write package.json

--- bin/yyProxy.js
+++ bin/yyProxy.js
@@ -4,15 +4,25 @@ let httpProxy = require("http-proxy");
 let proxy = httpProxy.createProxy({});
 let filePath = (process.argv && process.argv[2]) || "../proxyConfig";
 let path = require("path");
-let proxyConfig = require(path.join(filePath));
-console.log(process.argv);
-console.log(__dirname);
-console.log(proxyConfig);
-for(let obj of proxyConfig){
-    let server = http.createServer(function (req, res) {
-        proxy.web(req,res,{target:obj.targetUrl});
-    });
-    console.log(`port:${obj.port} targetUrl:${obj.targetUrl}`);
-    server.listen(obj.port);
-}
+// let proxyConfig = require(path.join(filePath));
+
+console.log(path.resolve(filePath));
+// console.log("process.env.pwd:")
+// console.log(process.env.pwd);
+// console.log(process.env.PWD);
+// console.log(path.join(process.env.pwd,filePath))
+// console.log(path.resolve(process.env.PWD,filePath))
+// console.log("process.env:")
+// console.log(process.env);
+// console.log(process.argv);
+// console.log(__dirname);
+// console.log(proxyConfig);
+
+// for(let obj of proxyConfig){
+//     let server = http.createServer(function (req, res) {
+//         proxy.web(req,res,{target:obj.targetUrl});
+//     });
+//     console.log(`port:${obj.port} targetUrl:${obj.targetUrl}`);
+//     server.listen(obj.port);
+// }
 console.log("yyProxy is running...");
